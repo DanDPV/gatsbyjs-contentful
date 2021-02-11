1489ca189706993e149cd83374a0da5ee1ae14c7
Daniel De Paz <vvelasquez@applaudostudios.com>
2021-02-10 19:55:46 -0600

    :wrench: Add sitemetadata and plugins config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,21 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: `Gatsby Contentful`,
+    description: `Connection to contentful example`,
+    author: `Vicente De Paz`,
+  },
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
+};
