2a2b09195a510b98f1ad909f487126eb7c209530
Daniel De Paz <vvelasquez@applaudostudios.com>
2021-02-11 18:57:05 -0600

    :sparkles: Add post template

---
+++ src/components/templates/post/index.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { graphql, Link } from 'gatsby';
+import { BLOCKS } from '@contentful/rich-text-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+import MainLayout from '../../layouts/MainLayout';
+import Routes from '../../../routes/routes';
+
+export const query = graphql`
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "MMMM Do. YYYY")
+      body {
+        raw
+        references {
+          contentful_id
+          title
+          file {
+            url
+          }
+        }
+      }
+    }
+  }
+`;
+
+type Props = {
+  data: {
+    contentfulBlogPost: {
+      title: string;
+      publishedDate: string;
+      body: {
+        raw: string;
+        references: {
+          contentful_id: string;
+          title: string;
+          file: {
+            url: string;
+          };
+        }[];
+      };
+    };
+  };
+};
+
+const Post = ({ data }: Props) => {
+  const { contentfulBlogPost } = data;
+
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        const id = node.data.target.sys.id;
+        const resource = contentfulBlogPost.body.references.find(
+          ref => ref.contentful_id === id
+        );
+        return (
+          <div className="flex justify-center my-4">
+            <img alt={resource.title} src={resource.file.url} />
+          </div>
+        );
+      },
+    },
+  };
+
+  return (
+    <MainLayout>
+      <div className="my-20 w-full">
+        <div className="flex justify-center">
+          <div className="w-3/4 lg:w-1/3">
+            <h1 className="text-3xl font-serif text-center">
+              {contentfulBlogPost.title}
+            </h1>
+            <p className="text-sm italic text-gray-500 text-center">
+              {contentfulBlogPost.publishedDate}
+            </p>
+            <hr />
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="bg-white rounded shadow-md hover:shadow-lg p-6 m-2 w-3/4 lg:w-2/3">
+            <div className="rich-content">
+              {documentToReactComponents(
+                JSON.parse(contentfulBlogPost.body.raw),
+                options
+              )}
+            </div>
+            <div className="text-center sm:text-left">
+              <Link
+                to={Routes.HOME}
+                className="hover:text-blue-400 text-gray-400"
+              >
+                <FontAwesomeIcon icon={faChevronLeft} />
+                {'\u00A0'}Go back
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Post;
